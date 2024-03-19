e35c4ee173bee82e89247d86e4903705837ff9ae
Amr Koritem <amr.koritem92@gmail.com>
2024-03-19 21:56:28 +0200

    [feat] mapping match model to match cell

--- grenta_task/Matches/MatchTableViewCell/MatchTableViewCell.swift
+++ grenta_task/Matches/MatchTableViewCell/MatchTableViewCell.swift
@@ -11,4 +11,23 @@ class MatchTableViewCell: UITableViewCell {
     @IBOutlet weak var homeTeamLabel: UILabel!
     @IBOutlet weak var awayTeamLabel: UILabel!
     @IBOutlet weak var resultOrTimeLabel: UILabel!
+    
+    func setLabels(homeTeam: String, awayTeam: String, resultOrTime: String?) {
+        homeTeamLabel.text = homeTeam
+        awayTeamLabel.text = awayTeam
+        resultOrTimeLabel.text = resultOrTime
+    }
+}
+
+extension MatchTableViewCell {
+    func setMatch(_ match: Match) {
+        let isDraw = match.score.winner == .draw
+        let result = match.winnerTeam?.name
+        let time = match.time
+        let isTime = result == nil && isDraw
+        setLabels(
+            homeTeam: match.homeTeam.name,
+            awayTeam: match.awayTeam.name,
+            resultOrTime: isTime ? time : result)
+    }
 }
